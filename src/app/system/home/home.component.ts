584337947016acebba9afffb58e5dceb1f62af35
darinasmile <daria.pankratova@clockwise.software>
2021-04-27 19:01:03 +0300

    connect to component

--- src/app/system/home/home.component.ts
+++ src/app/system/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/models/models';
+import { UsersService } from 'src/app/shared/services/users.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  users: User[]
 
-  constructor() { }
+  constructor(
+    private userService: UsersService
+  ) { }
 
   ngOnInit(): void {
+    this.userService.getUsers()
+      .subscribe((users: User[]) => {
+        this.users = users;
+      });
   }
 
 }
